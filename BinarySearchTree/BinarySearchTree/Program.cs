58cdeb2d4959c8e1ffed2a03c94f29292d3796c9
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-08-01 22:08:36 +0530

    created Binary Search Tree

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace BinarySearchTree_Demo
+﻿using BinarySearchTree;
+using System;
+
+
+namespace BinarySearchTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to The Binary Search  Tree Program");
+            Console.WriteLine("Welcome to The Binary Search Tree Program");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
+            Console.ReadKey();
         }
     }
-}
+    }
+
