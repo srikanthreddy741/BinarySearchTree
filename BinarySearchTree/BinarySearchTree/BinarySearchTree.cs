cd90ee7efa0fe0a84dbf5ee9144809e97f78f446
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-08-01 22:17:08 +0530

    Multiple Binary Search Tree Nodes

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -6,60 +6,60 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
- 
-        class BinarySearchTree<T> where T : IComparable<T>
+
+    class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+        public BinarySearchTree(T nodeData)
         {
-            public T NodeData { get; set; }
-            public BinarySearchTree<T> leftTree { get; set; }
-            public BinarySearchTree<T> rightTree { get; set; }
-            public BinarySearchTree(T nodeData)
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0) // "A".CompareTo("a")
             {
-                this.NodeData = nodeData;
-                this.rightTree = null;
-                this.leftTree = null;
-            }
-            int leftCount = 0, rightCount = 0;
-            bool result = false;
-            public void Insert(T item)
-            {
-                T currentNodeValue = this.NodeData;
-                if ((currentNodeValue.CompareTo(item)) > 0) // "A".CompareTo("a")
+                if (this.leftTree == null)
                 {
-                    if (this.leftTree == null)
-                    {
-                        this.leftTree = new BinarySearchTree<T>(item);
-                    }
-                    else
-                        this.leftTree.Insert(item);
+                    this.leftTree = new BinarySearchTree<T>(item);
                 }
                 else
-                {
-                    if (this.rightTree == null)
-                    {
-                        this.rightTree = new BinarySearchTree<T>(item);
-                    }
-                    else
-                        this.rightTree.Insert(item);
-                }
+                    this.leftTree.Insert(item);
             }
-            public void Display()
+            else
             {
-                if (this.leftTree != null)
+                if (this.rightTree == null)
                 {
-                    this.leftCount++;
-                    this.leftTree.Display();
+                    this.rightTree = new BinarySearchTree<T>(item);
                 }
-                Console.WriteLine(this.NodeData.ToString());
-                if (this.rightTree != null)
-                {
-                    this.rightCount++;
-                    this.rightTree.Display();
-                }
-            }
-            public void GetSize()
-            {
-                Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+                else
+                    this.rightTree.Insert(item);
             }
         }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+    }
     }
 
--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -12,6 +12,16 @@ namespace BinarySearchTree
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
             binarySearch.Insert(30);
             binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
             binarySearch.Display();
             binarySearch.GetSize();
             Console.ReadKey();
