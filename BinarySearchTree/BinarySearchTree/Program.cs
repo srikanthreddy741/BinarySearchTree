e86bf5c0f00746ad2e409b14307275035561216a
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-08-01 21:57:21 +0530

    Added WellCome Mesg

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Binary Search  Tree Program");
+        }
+    }
+}
